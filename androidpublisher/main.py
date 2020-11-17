b30b02e2acc84e164a7bf0621c59840ac53d5ad2
leynier <leynier41@gmail.com>
2020-11-17 18:03:40 -0500

    Separate upload method from upload command

--- androidpublisher/main.py
+++ androidpublisher/main.py
@@ -3,10 +3,8 @@ from mimetypes import add_type
 from pathlib import Path
 
 import typer
-from apiclient.discovery import build
-from httplib2 import Http
-from oauth2client.client import AccessTokenRefreshError
-from oauth2client.service_account import ServiceAccountCredentials
+
+from .upload import upload
 
 
 class Track(str, Enum):
@@ -28,8 +26,8 @@ def callback():
     """
 
 
-@app.command()
-def upload(
+@app.command(name="upload")
+def upload_command(
     package_name: str,
     aab_file: Path = typer.Option(
         "app.aab",
@@ -51,75 +49,9 @@ def upload(
         resolve_path=True,
     ),
 ):
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        str(json_key),
-        scopes="https://www.googleapis.com/auth/androidpublisher",
+    upload(
+        package_name=package_name,
+        aab_file=str(aab_file),
+        track=track.value,
+        json_key=str(json_key),
     )
-    http = Http()
-    http = credentials.authorize(http)
-
-    service = build("androidpublisher", "v3", http=http)
-
-    try:
-        edit_request = service.edits().insert(
-            body={},
-            packageName=package_name,
-        )
-        result = edit_request.execute()
-        edit_id = result["id"]
-
-        aab_response = (
-            service.edits()
-            .bundles()
-            .upload(
-                editId=edit_id,
-                packageName=package_name,
-                media_body=str(aab_file),
-            )
-            .execute()
-        )
-
-        version_code = str(aab_response["versionCode"])
-
-        typer.echo(f"Version code {version_code} has been uploaded")
-
-        track_response = (
-            service.edits()
-            .tracks()
-            .update(
-                editId=edit_id,
-                track=track.value,
-                packageName=package_name,
-                body={
-                    "releases": [
-                        {
-                            "name": f"Version {version_code}",
-                            "versionCodes": [version_code],
-                            "status": "completed",
-                        }
-                    ]
-                },
-            )
-            .execute()
-        )
-
-        track_message = f"Track {track_response['track']} is set with "
-        track_message += f"releases: {track_response['releases']}"
-
-        typer.echo(track_message)
-
-        commit_request = (
-            service.edits()
-            .commit(
-                editId=edit_id,
-                packageName=package_name,
-            )
-            .execute()
-        )
-
-        typer.echo(f"Edit \"{commit_request['id']}\" has been committed")
-
-    except AccessTokenRefreshError:
-        error_message = "The credentials have been revoked or expired, "
-        error_message += "please re-run the application to re-authorize"
-        typer.echo(error_message)
---
+++ androidpublisher/upload.py
@@ -0,0 +1,85 @@
+import typer
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def upload(
+    package_name: str,
+    aab_file: str = "app.aab",
+    track: str = "internal",
+    json_key: str = "credential.json",
+):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        json_key,
+        scopes="https://www.googleapis.com/auth/androidpublisher",
+    )
+    http = Http()
+    http = credentials.authorize(http)
+
+    service = build("androidpublisher", "v3", http=http)
+
+    try:
+        edit_request = service.edits().insert(
+            body={},
+            packageName=package_name,
+        )
+        result = edit_request.execute()
+        edit_id = result["id"]
+
+        aab_response = (
+            service.edits()
+            .bundles()
+            .upload(
+                editId=edit_id,
+                packageName=package_name,
+                media_body=aab_file,
+            )
+            .execute()
+        )
+
+        version_code = str(aab_response["versionCode"])
+
+        typer.echo(f"Version code {version_code} has been uploaded")
+
+        track_response = (
+            service.edits()
+            .tracks()
+            .update(
+                editId=edit_id,
+                track=track,
+                packageName=package_name,
+                body={
+                    "releases": [
+                        {
+                            "name": f"Version {version_code}",
+                            "versionCodes": [version_code],
+                            "status": "completed",
+                        }
+                    ]
+                },
+            )
+            .execute()
+        )
+
+        track_message = f"Track {track_response['track']} is set with "
+        track_message += f"releases: {track_response['releases']}"
+
+        typer.echo(track_message)
+
+        commit_request = (
+            service.edits()
+            .commit(
+                editId=edit_id,
+                packageName=package_name,
+            )
+            .execute()
+        )
+
+        typer.echo(f"Edit \"{commit_request['id']}\" has been committed")
+
+    except AccessTokenRefreshError:
+        error_message = "The credentials have been revoked or expired, "
+        error_message += "please re-run the application to re-authorize"
+        typer.echo(error_message)
